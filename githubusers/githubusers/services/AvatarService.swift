76b8887a499445811ff9ef32c232db4b17d5295c
Brylle <brylled18@gmail.com>
2020-12-21 02:08:15 +0800

    AvatarService

---
+++ githubusers/githubusers/services/AvatarService.swift
@@ -0,0 +1,16 @@
+//
+//  AvatarService.swift
+//  githubusers
+//
+//  Created by Brylle Seraspe on 12/20/20.
+//
+
+import Foundation
+
+class AvatarService {
+    
+    static let shared = AvatarService()
+    
+    
+    
+}
